0c539f04a300259c2682ff89bd71bb98291e7478
Nery2004 <123770230+Nery2004@users.noreply.github.com>
2025-05-25 19:27:17 -0600

    C

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Calculator from '../Components/Calculator/index.jsx'
-import '../styles/src/Calculator.css'
+import '../src/styles/Calculator.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
