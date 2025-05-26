3ed1753ef7a5ac357340d22f554897ece3ca2f3b
Nery2004 <123770230+Nery2004@users.noreply.github.com>
2025-05-25 21:54:03 -0600

    c

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Proyecto-1-Calculadora/dist/',
+  base: '/Proyecto-1-Calculadora/',
   plugins: [react()],
   build: {
     outDir: 'dist'
