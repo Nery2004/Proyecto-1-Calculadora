5b4bcf0ea38e165fe8924686509875bf5812bcbb
Nery2004 <123770230+Nery2004@users.noreply.github.com>
2025-05-25 21:31:07 -0600

    c

--- Components/Calculator/index.jsx
+++ Components/Calculator/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Display from '../Display'
 import Keypad from '../Keypad'
-import useCalculator from '../../hooks/useCalculator'
+import useCalculator from '../../hooks/UseCalculator'
 
 const Calculator = () => {
   const { display, handleButtonClick } = useCalculator()
